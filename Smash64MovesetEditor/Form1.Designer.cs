4922705f033be36588334fd386f4298bba5b1aaf
Qapples <armand.scotty@yahoo.com>
2020-01-07 21:27:25 -0500

    Upload

---
+++ Smash64MovesetEditor/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Smash64MovesetEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.OutputBox = new System.Windows.Forms.TextBox();
+            this.HexCodeButton = new System.Windows.Forms.Button();
+            this.CodeHexButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // InputBox
+            // 
+            this.InputBox.Location = new System.Drawing.Point(12, 43);
+            this.InputBox.Multiline = true;
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(258, 274);
+            this.InputBox.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Input";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(110, 320);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Output";
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.Location = new System.Drawing.Point(12, 336);
+            this.OutputBox.Multiline = true;
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.Size = new System.Drawing.Size(258, 278);
+            this.OutputBox.TabIndex = 4;
+            // 
+            // HexCodeButton
+            // 
+            this.HexCodeButton.Location = new System.Drawing.Point(302, 71);
+            this.HexCodeButton.Name = "HexCodeButton";
+            this.HexCodeButton.Size = new System.Drawing.Size(126, 48);
+            this.HexCodeButton.TabIndex = 5;
+            this.HexCodeButton.Text = "Convert hex into code";
+            this.HexCodeButton.UseVisualStyleBackColor = true;
+            this.HexCodeButton.Click += new System.EventHandler(this.HexCodeButton_click);
+            // 
+            // CodeHexButton
+            // 
+            this.CodeHexButton.Location = new System.Drawing.Point(302, 226);
+            this.CodeHexButton.Name = "CodeHexButton";
+            this.CodeHexButton.Size = new System.Drawing.Size(126, 48);
+            this.CodeHexButton.TabIndex = 6;
+            this.CodeHexButton.Text = "Convert code into hex";
+            this.CodeHexButton.UseVisualStyleBackColor = true;
+            this.CodeHexButton.Click += new System.EventHandler(this.CodeHexButton_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(451, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.openToolStripMenuItem.Text = "Open             Ctrl + O";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.saveToolStripMenuItem.Text = "Save               Crtl + S";
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 622);
+            this.Controls.Add(this.CodeHexButton);
+            this.Controls.Add(this.HexCodeButton);
+            this.Controls.Add(this.OutputBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.InputBox);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Smash 64 Moveset Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox OutputBox;
+        private System.Windows.Forms.Button HexCodeButton;
+        private System.Windows.Forms.Button CodeHexButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
---
+++ Smash64MovesetEditor/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Smash64MovesetEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.Filter = "Text Files|*.txt|BIN files|*.bin";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HexCodeButton_click(object sender, EventArgs e)
+        {
+
+        }
+
+        public Task<string> ConvertCodeToHex(string input)
+        {
+            string[] commands = input.Split('\n');
+            string output = "";
+
+            foreach (string command in commands)
+            {
+                //Split the command by spaces. The first element is the command. The next elements are the parameters. The first element of
+                //Parameters is -1.
+                string[] splitCmd = command.Split(' ');
+                string opcode = splitCmd[0];
+
+                uint[] parameters = splitCmd.Select(e => uint.TryParse(e, out _) ? uint.Parse(e) : 0).ToArray();
+                uint hex;
+
+                switch (opcode)
+                {
+                    case "Hitbox":
+                        //HITBOX parameters in order: ID#1 DMG BKB FKB KBS Angle Bone X Y Z GT AT SD Clang Size Effect SoundType SoundLevel
+                        hex = (0xC000000 | (parameters[1] * 0x800000)); //ID 1
+                        hex = hex | (parameters[7] << 13); //Bone
+                        hex = hex | (parameters[2] << 5); //Damage
+                        hex = hex | (parameters[14] << 4); //Clang
+                        parameters[16] = parameters[16] == 5 ? 7 : parameters[16]; //Effect. Change 5 into 7
+                        hex += parameters[16]; //add effect
+                        output = $"{hex:X8} ";
+
+                        hex = 0 | (parameters[15] << 17); //Size
+                        hex = hex | (parameters[8] & 0xFFFF); //X Position
+                        output += $"{hex:X8} ";
+
+                        hex = (parameters[9] << 16) & 0xFFFF0000; //Y Position
+                        hex = hex | (parameters[10] & 0xFFFF); //Z position
+                        output += $"{hex:X8} ";
+
+                        hex = parameters[6] << 22; //Angle
+                        hex += parameters[5] << 12; //Knockback Scaling
+                        hex += parameters[4] * 4; //Fixed knocback
+                        hex += parameters[11] * 2; //Grounded targets
+                        hex += parameters[12]; //Aerial targets
+                        output += $"{hex:X8} ";
+
+                        hex = parameters[13] << 24; //Sheild Damage
+                        hex += (parameters[17] * 2) << 20; //Sound level
+                        hex += (parameters[18] * 2) << 16; //Sound type
+                        hex += parameters[3] << 7;
+                        output += $"{hex:X8} ";
+                        break;
+                    case "EndHitboxes":
+                        //No parameters
+                        output += $"{0x18000000:X8}";
+                        break;
+                    case "ReviveHitbox":
+                        //Paramaters: Main
+                        output += $"{0x2C000000 + parameters[1]:X8}";
+                        break;
+                    case "Wait":
+                        //Paramters: Main
+                        output += $"{0x04000000 + parameters[1]:X8}";
+                        break;
+                    case "After":
+                        //Paramaters: Main
+                        output += $"{0x08000000 + parameters[1]:X8}";
+                        break;
+                    case "SFX14":
+                        //Paramaters: Main
+                        output += $"{0x38000000 + parameters[1]:X8}";
+                        break;
+                    case "SFX19":
+                        //Paramaters: Main
+                        output += $"{0x4C000000 + parameters[1]:X8}";
+                        break;
+                    case "GenericVoice":
+                        //No parameters
+                        output += $"{0x50000000:X8}";
+                        break;
+                    case "Voice":
+                        //Paramaters: Main
+                        output += $"{0x44000000 + parameters[1]:X8}";
+                        break;
+                    case "BeginLoop":
+                        //Paramaters: Main
+                        output += $"{0x80000000 + parameters[1]:X8}";
+                        break;
+                    case "EndLoop":
+                        //Paramaters: None
+                        output += $"{0x84000000:X8}";
+                        break;
+                    case "SetFlag":
+                        //Paramaters: Main
+                        output += $"{0x58000000 + parameters[1]:X8}";
+                        break;
+                    case "SetHurtboxState":
+                        //Paramaters: Main
+                        output += $"{0x74000000 + parameters[1]:X8}";
+                        break;
+                    case "ResetHurtboxState":
+                        //Paramaters: Main
+                        output += $"{0x6C000000 + parameters[1]:X8}";
+                        break;
+                    case "SetTextureForm":
+                        //Paramaters: Main
+                        output += $"{0xAC000000 + parameters[1]:X8}";
+                        break;
+                    case "SetSlopeContourState":
+                        //Paramaters: Main
+                        output += $"{0xBC000000 + parameters[1]:X8}";
+                        break;
+                    case "ApplyThrow":
+                        //Paramaters: Main
+                        output += $"{0x5C000000 + parameters[1]:X8}";
+                        break;
+                    case "ThrowData":
+                        //Paramaters: Main
+                        output += $"{0x30000000 + parameters[1]:X8}";
+                        break;
+                    case "CreateProp":
+                        //Paramaters: Main
+                        output += $"{0x54000000 + parameters[1]:X8}";
+                        break;
+                    case "Goto":
+                        //Paramaters: Main
+                        output += $"{0x90000000 + parameters[1]:X8}";
+                        break;
+                    case "Return":
+                        //Paramaters: None
+                        output += $"{0x8C000000:X8}";
+                        break;
+                    case "Subroutine":
+                        //Paramters: Main
+                        output += $"{0x88000000 + parameters[1]:X8}";
+                        break;
+
+                }
+            }
+
+            return Task.FromResult(String.Format(output));
+        }
+
+        private async void CodeHexButton_Click(object sender, EventArgs e)
+        {
+            OutputBox.Text = String.Format("{0:X}\n", await ConvertCodeToHex(InputBox.Text));
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Open the openfile dialog
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Open the file and read it
+                Stream stream = openFileDialog1.OpenFile();
+                byte[] bytes = new byte[stream.Length];
+
+                for (int i = 0; i < stream.Length; i++)
+                {
+                    await stream.ReadAsync(bytes, 0, (int)stream.Length);
+                }
+
+                if (openFileDialog1.FileName.Contains(".txt"))
+                {
+                    InputBox.Text = Encoding.ASCII.GetString(bytes);
+                }
+                else
+                {
+                    string output = "";
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+                        //Every four bytes, make a space. Make a new line every 16 bytes
+                        if (i % 4 == 0)
+                        {
+                            output += " ";
+                        }
+                        if (i % 16 == 0)
+                        {
+                            output += Environment.NewLine;
+                        }
+
+                        output += $"{bytes[i]:X2}";
+                    }
+
+                    InputBox.AppendText(output);
+                }
+            }
+        }
+
+        /*
+        public string ConvertHexToCode(byte[] input)
+        {
+            
+        }*/
+    }
+}
